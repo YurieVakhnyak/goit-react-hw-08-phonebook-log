758dafb1b474d2327325b2286338084564937c55
YurieVakhnyak <110679725+YurieVakhnyak@users.noreply.github.com>
2023-03-28 14:38:26 +0000

    Deploying to gh-pages from @ YurieVakhnyak/goit-react-hw-08-phonebook-log@95563a7371788264adaeaceb99419b13f87bdd3a 🚀

--- static/js/75.6f166482.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[75],{6075:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var c=n(184),i={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function r(){return(0,c.jsx)("div",{style:i.container,children:(0,c.jsx)("h1",{style:i.title,children:"Contacts phonebook welcome page"})})}}}]);
-//# sourceMappingURL=75.6f166482.chunk.js.map
